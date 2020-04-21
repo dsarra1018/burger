f91e6ed32d3b590995bbefa51a213f117afd3abb
Darren Sarra <adriansarra18@gmail.com>
2020-04-21 17:18:34 -0400

    Initiate MySQL Connection.

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,20 @@
 // Dependencies
 const mysql = require('mysql');
+
+// MySQL DB Connection Information
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+});
+
+// Initiate MySQL Connection
+connection.connect((err) => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
