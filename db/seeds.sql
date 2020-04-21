62ee852bcd9355a9a69001703a1857d614013fd8
Darren Sarra <adriansarra18@gmail.com>
2020-04-21 16:46:51 -0400

    Populate the 'burgers' table using the seeds.sql file.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+-- Populating the "burgers" table --
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Spicy Chicken Sammich', FALSE), ('BIG MAC', FALSE), ('ShackBurger', FALSE);
