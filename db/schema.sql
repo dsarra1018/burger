fa56ed8924af20f5354961233a7d9037ab97e953
Darren Sarra <adriansarra18@gmail.com>
2020-04-21 16:35:44 -0400

    Created 'burgers' table within the 'burgers_db'. Contains keys: id, burger_name, and devoured with id being primary key.

--- db/schema.sql
+++ db/schema.sql
@@ -5,3 +5,10 @@ CREATE DATABASE burgers_db;
 -- Makes it so all of the following code will affect burgers_db --
 USE burgers_db;
 
+-- Creates the table "burgers" within burgers_db --
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR(30),
+    devoured BOOLEAN,
+    PRIMARY KEY(id)
+);
