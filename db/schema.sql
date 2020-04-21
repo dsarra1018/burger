513c0163e8648d146b581e93d49f44001b63043d
Darren Sarra <adriansarra18@gmail.com>
2020-04-21 16:30:33 -0400

    Initialized schema.sql file. Drops 'burgers_db' database if it exists. Creates the database. Start using 'burgers_db' database.

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- Drops the burgers_db if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+-- Create the "burgers_db" database --
+CREATE DATABASE burgers_db;
+-- Makes it so all of the following code will affect burgers_db --
+USE burgers_db;
+
