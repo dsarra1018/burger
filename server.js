4490097401bdbee95f2a77f97ce00d1f7d2df13d
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 12:07:31 -0400

    Re-oriented server configuration.

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers/burgers_controller');
-const path = require('path');
 
 // create an app
 const app = express();
 
 // Setting port
 const PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the 'public' directory in the application directory
+app.use(express.static('public'));
 
 // Handle JSON
 app.use(express.urlencoded({extended: true}));
@@ -18,11 +20,8 @@ app.use(express.json());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// 'public' folder is user-facing
-app.use(express.static(path.join(__dirname, '/public')));
-
 app.use('/', routes);
 
 app.listen(PORT, () => {
     console.log("App is listening on PORT " + PORT);
-})
+});
