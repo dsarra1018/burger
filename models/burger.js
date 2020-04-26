1e73916cd14a85ab424e772ec35de0beec07fa2e
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 14:30:23 -0400

    Calling the insertOne ORM function inside the models/burger.js file.

--- models/burger.js
+++ models/burger.js
@@ -2,9 +2,20 @@
 const orm = require('../config/orm');
 
 let burger = {
+
+    // Calling selectAll ORM function
     selectAll: (cb) => {
-        orm.all('burgers', (res) => {
+        orm.selectAll('burgers', (res) => {
             cb(res);
         });
-    }
+    },
+
+    // Calling insertOne ORM function
+    insertOne: (cols, vals, cb) => {
+        orm.insertOne('burgers', cols, vals, (res) => {
+            cb(res);
+        });
+    },
+
+
 }
