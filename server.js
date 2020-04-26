c59e969c5cb5d304b9cbafa89c133baa5ca05d75
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 03:00:00 -0400

    Setup the middleware. The rendering engine is handlebars.

--- server.js
+++ server.js
@@ -10,3 +10,6 @@ const app = express();
 // Setting port
 const PORT = process.env.PORT || 8080;
 
+// Middleware-Rendering Engine: Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
