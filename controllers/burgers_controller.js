652599ac9346a9ad72c5b8f88101edd6f4e91076
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 14:37:04 -0400

    Setup get routes for the selectAll function.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,14 @@
 // Dependencies
 const router = require('express').Router();
-const burgers = require('../models/burger');
+const burger = require('../models/burger');
+
+// Routes
+router.get("/", (req, res) => {
+    burger.selectAll( (data) => {
+        let hbsObj = {
+            burgers: data
+        };
+        console.log(hbsObj);
+        res.render('index', hbsObj);
+    });
+});
