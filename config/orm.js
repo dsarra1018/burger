fbdb1faaa229ab1c56c86e121b04031ddd4fb928
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 12:47:30 -0400

    Created a helper function that would convert object key/value pairs to SQL syntax.

--- config/orm.js
+++ config/orm.js
@@ -12,6 +12,25 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
+// Helper function to convert objects key/value pairs to SQL syntax
+function objToSql(ob) {
+    let arr = [];
+
+    for (let key in ob) {
+        let value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+                value = '' + value + '';
+            }
+
+            arr.push(key + '=' + value);
+        }
+    }
+
+    return arr.toString();
+}
+
 let orm = {
 
     selectAll: (table) => {
