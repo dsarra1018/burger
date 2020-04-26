b091b76945f78a29cbf5502a4cad27a1b78f3f47
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 14:48:23 -0400

    Setup put routes for updateOne function.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,5 +22,20 @@ router.post('/api/burgers', (req, res) => {
     });
 });
 
+router.put('/api/burgers/:id', (req, res) => {
+    let condition = 'id = ' + req.params.id;
 
+    console.log('condition', condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, (result) => {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
+});
 });
