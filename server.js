3cad038fdec69f237beffb695faa3e967977325d
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 02:48:59 -0400

    Imported dependencies.

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 // Dependencies 
 const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/burgers_controller');
+const path = require('path');
