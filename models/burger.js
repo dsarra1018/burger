0e9daa463347c546f525df95c17e45930552fcc5
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 14:25:57 -0400

    Calling the selectAll() ORM function inside the model/burger.js file.

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,10 @@
 // Dependencies
 const orm = require('../config/orm');
 
+let burger = {
+    selectAll: (cb) => {
+        orm.all('burgers', (res) => {
+            cb(res);
+        });
+    }
+}
