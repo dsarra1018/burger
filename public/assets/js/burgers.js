b6dc9080cd7c425bfd7c7c2110b303bb59edbdf7
Darren Sarra <adriansarra18@gmail.com>
2020-04-26 22:44:51 -0400

    Created a function that would handle devouring burgers.

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.devour-form').on('submit', function(event) {
+        event.preventDefault();
+
+        let burger_id = $(this).children('.burger_id').val();
+        $.ajax({
+            method: 'PUT',
+            url: '/burgers/' + burger_id
+        }).then(function(data) {
+            location.reload();
+        })
+    })
+})
